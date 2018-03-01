6932580fde0206a555aa3e7f574849250cccbe5c
ClaraMackay <clara.mackay@ocsbstudent.ca>
2018-03-01 10:46:17 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,8 @@
+--Title: MovingImages
+-- Name: Clara Mackay
+-- Course: ICS2O
+-- This program displays a beetleship moving across the screen as well as another 
+-- object moving in a different direction. 
+
+-- hide the status bar
+display.setStatus
